5bca5560f5b47529a471387f562f28cd2839aea9
jorgeCasalini <joshcasalini@gmail.com>
2025-06-01 22:16:48 -0300

    Create Pipe Validation

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,13 +1,53 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, ParseBoolPipe, ParseIntPipe, Query, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { ValidateUserPipe } from './pipes/validate-user/validate-user.pipe';
 
 @Controller('')
 export class HelloController {
 
     @Get('/')
     index(@Req() request: Request, @Res() response: Response) {
-       response.status(200).json({
-        message: 'Hello World',
-       })
+        response.status(200).json({
+            message: 'Hello World',
+        })
     }
+
+    @Get('new')
+    @HttpCode(201)
+    somethingNew() {
+        return 'Something New'
+    }
+
+    @Get('notfound')
+    @HttpCode(404)
+    notFoundPage() {
+        return '404 not found'
+    }
+
+    @Get('error')
+    @HttpCode(500)
+    errorPage() {
+        return 'Error Route!'
+    }
+
+    @Get('ticket/:num')
+    getNumber(@Param('num', ParseIntPipe) num: number) {
+        return num + 14;
+    }
+
+    @Get('active/:status')
+    isUserActive(@Param('status', ParseBoolPipe) status: boolean) {
+        console.log(typeof status)
+        return status
+    }
+
+    @Get('greet')
+    greet(@Query(ValidateUserPipe) query: {name: string, age: number}) {
+        console.log(typeof query.name)
+        console.log(typeof query.age)
+        return `Hello ${query.name}, you are ${query.age} years old`;
+    
+    }
+    
 }
+ 
